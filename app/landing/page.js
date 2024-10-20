3a5672b75764bc57b0f4f84f100a800fbc68c8c6
Teddi <tedthedude27@gmail.com>
2024-10-20 15:41:19 -0500

    update landing page

--- app/landing/page.js
+++ app/landing/page.js
@@ -1,9 +1,20 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Landing() {
   return (
     <div className="landing flex flex-col items-center min-h-screen p-4">
-      <h1 className="text-xl">Landing</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl text-indigo-700">Pathfinder</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-lg text-muted-foreground max-w-screen-md">
+            If you're looking for volunteering and internship opportunities,
+            Pathfinder is your catalog. If you're looking for volunteers or
+            bright people willing to learn, Pathfinder is your catalog.
+          </p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
