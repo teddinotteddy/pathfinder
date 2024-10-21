055aae45a0859c651672a21230f6cce997c7c90a
KunlingW <kunling802@outlook.com>
2024-10-20 22:43:15 -0500

    Added flowing background for landing page
    - flowing background for landing page, replaced the original doodle#1

--- app/landing/page.js
+++ app/landing/page.js
@@ -3,11 +3,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 export default function Landing() {
   return (
     <div className="landing flex flex-col items-center min-h-screen p-4">
-      <Card>
+      <Card className="p-48 text-center">
         <CardHeader>
-          <CardTitle className="text-2xl text-indigo-700">Pathfinder</CardTitle>
+          <CardTitle className="text-3xl text-indigo-700 italic underline">Welcome to Pathfinder</CardTitle>
         </CardHeader>
         <CardContent>
+          <h1 className="text-xl">Your Opportunities & Internships Hub</h1>
           <p className="text-lg text-muted-foreground max-w-screen-md">
             If you're looking for volunteering and internship opportunities,
             Pathfinder is your catalog. If you're looking for volunteers or
