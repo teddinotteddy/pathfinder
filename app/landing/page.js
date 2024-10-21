2d2ec37890607069c58bcb1b6cd43873b0c346b6
KunlingW <kunling802@outlook.com>
2024-10-20 22:51:54 -0500

    change font size, minor

--- app/landing/page.js
+++ app/landing/page.js
@@ -5,11 +5,11 @@ export default function Landing() {
     <div className="landing flex flex-col items-center min-h-screen p-4">
       <Card className="p-48 text-center">
         <CardHeader>
-          <CardTitle className="text-3xl text-indigo-700 italic underline">Welcome to Pathfinder</CardTitle>
+          <CardTitle className="text-5xl text-indigo-700 italic underline">Welcome to Pathfinder...</CardTitle>
         </CardHeader>
         <CardContent>
-          <h1 className="text-xl">Your Opportunities & Internships Hub</h1>
-          <p className="text-lg text-muted-foreground max-w-screen-md">
+          <h1 className="text-2xl">Your Opportunities & Internships Hub</h1>
+          <p className="text-xl text-muted-foreground max-w-screen-md">
             If you're looking for volunteering and internship opportunities,
             Pathfinder is your catalog. If you're looking for volunteers or
             bright people willing to learn, Pathfinder is your catalog.
